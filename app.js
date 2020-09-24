4918c130e72d395718bdb3c647c5c655a99f1a89
eduardo reta rj <https://github.com/Eddie-Reta>
2020-09-23 22:26:50 -0400

    User can now request weather data.

--- app.js
+++ app.js
@@ -2,11 +2,23 @@ const express = require("express");
 
 const https = require("https");
 
+const bodyParser = require("body-parser");
+
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", function(req, res) {
-    
-    const url = "https://api.openweathermap.org/data/2.5/weather?zip=78586&units=imperial&appid=c93a11784166c017d25d3730cc0f67be"
+    res.sendFile(__dirname + "/client/index.html")
+});
+
+app.post("/", function(req, res){
+   
+    const query = req.body.cityName;
+    const apiKey = "c93a11784166c017d25d3730cc0f67be";
+    const units = "imperial";    
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query + "&units="+  units +"&appid=" + apiKey;
+    const city = query[0].toUpperCase() + query.slice(1)
 
     //http request is native to node 
     https.get(url,function(response){
@@ -19,13 +31,19 @@ app.get("/", function(req, res) {
             const temp = weatherData.main.temp;
 
             const weatherDescription = weatherData.weather[0].description
-            console.log(weatherDescription );
+            
+            const icon = weatherData.weather[0].icon;
 
+            const imageUrl = "http://openweathermap.org/img/wn/"+ icon +"@2x.png";
+            //You can have multiple res.write
+            res.write("<p>The weather is currently " + weatherDescription + ".</p>");                
+            res.write("<h1>The temprature in "+ city +" is " + temp + " degrees ferenheit.</h1>")
+            res.write("<img src="+ imageUrl +" alt="+ weatherDescription + ">")
+            res.send()
             //json.stringify will turn a javascript object into a string
-        })
-    });
+       })
+   });
 
-    res.send("Server is up and running.");
 });
 
 app.listen(3000, function(){
